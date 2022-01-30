debce154c36f7359ac6c1317c00f3bbc384aab5d
zal-byte <rizal.solehudin45@gmail.com>
2022-01-30 21:53:46 +0700

    B

--- api.php
+++ api.php
@@ -8,9 +8,10 @@
 	Handler::getInstance();
 	USER::getInstance();
 	ABSEN::getInstance();
+
 	Handler::$context = 'API';
 
-	!empty($_SERVER['REQUEST_METHOD']) ? ( $_SERVER['REQUEST_METHOD'] == 'POST' ? post($_POST) : ($_SERVER['REQUEST_METHOD'] == 'GET' ? get($_GET) : die("request_method error") ) ) : die("Error");
+	!empty($_SERVER['REQUEST_METHOD']) ? ( $_SERVER['REQUEST_METHOD'] == 'POST' ? post($_POST) : ($_SERVER['REQUEST_METHOD'] == 'GET' ? get($_GET) : die("request_method error") ) ) : Handler::HandlerError("API Error");
 
 
 	function post( $post )
